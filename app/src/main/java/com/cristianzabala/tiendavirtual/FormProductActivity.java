57e7cee15774e29ae6fbf56dc4ac37d3d91f3b53
Hiperion09 <juanesteban.arenas2002@gmail.com>
2024-03-06 16:44:29 -0500

    si

---
+++ app/src/main/java/com/cristianzabala/tiendavirtual/FormProductActivity.java
@@ -0,0 +1,14 @@
+package com.cristianzabala.tiendavirtual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_product);
+    }
+}
